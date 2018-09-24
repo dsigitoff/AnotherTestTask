e074117fc8ee2be58da39c871d11087c5f265db8
dsigitoff <golovsha@gmail.com>
2018-09-24 12:30:09 +0300

    swiper added

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <header class="header">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/">About Us</router-link> |
-        <router-link to="/contact">Contact Us</router-link>
-      </header>
+    <div id="app">
+        <div id="nav">
+            <header class="header">
+                <router-link to="/">Home</router-link>
+                |
+                <router-link to="/">About Us</router-link>
+                |
+                <router-link to="/contact">Contact Us</router-link>
+            </header>
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 1200px;
-}
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    body {
+        padding: 0;
+        margin: 0;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-  .header {
-    position: fixed;
-    width: 100%;
-  }
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        height: 1200px;
+    }
+
+    #nav {
+
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+        padding: 20px;
+        text-decoration: none;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #42b983;
+    }
+
+    .header {
+        width: 100%;
+        border-bottom: 0.3px solid #2c3e50;
+        padding: 20px;
+    }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,32 +1,55 @@
 <template>
-  <div class="hello">
-
-  </div>
+    <div class="hello">
+        <swiper :options="swiperOption">
+            <swiper-slide>Slide 1</swiper-slide>
+            <swiper-slide>Slide 2</swiper-slide>
+            <swiper-slide>Slide 3</swiper-slide>
+            <swiper-slide>Slide 4</swiper-slide>
+            <swiper-slide>Slide 5</swiper-slide>
+            <swiper-slide>Slide 6</swiper-slide>
+            <swiper-slide>Slide 7</swiper-slide>
+            <swiper-slide>Slide 8</swiper-slide>
+            <swiper-slide>Slide 9</swiper-slide>
+            <swiper-slide>Slide 10</swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
+    export default {
+        data() {
+            return {
+                swiperOption: {
+                    slidesPerView: 3,
+                    spaceBetween: 30,
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true
+                    }
+                }
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
